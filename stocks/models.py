8117745181536c98a4341ffb5876370865825c0f
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-29 18:56:56 -0500

    Create `StockSymbol` model

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class StockSymbol(models.Model):
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
