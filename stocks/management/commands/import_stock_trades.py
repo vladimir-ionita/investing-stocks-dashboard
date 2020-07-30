f1cb8a6b7e66c13c1b370d32ce4d86dc025be1e6
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-30 09:35:27 -0500

    Add arguments to the `import_stock_trades` command

--- stocks/management/commands/import_stock_trades.py
+++ stocks/management/commands/import_stock_trades.py
@@ -4,5 +4,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Import stock trades from email"
 
+    def add_arguments(self, parser):
+        parser.add_argument('email', help="email address to be used for import")
+        parser.add_argument('password', help="email password")
+        parser.add_argument('search_criteria', help="email search criteria according to RFC3501 section 6.4.4")
+
     def handle(self, *args, **options):
         pass
