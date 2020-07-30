f35c6714adc15dcffa318fa6c0ec3232c42e7149
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-29 19:03:06 -0500

    Create `StockTrade` model

--- stocks/models.py
+++ stocks/models.py
@@ -7,3 +7,15 @@ class StockSymbol(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class StockTrade(models.Model):
+    stock = models.ForeignKey(StockSymbol, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    share_price = models.DecimalField(decimal_places=2, max_digits=7+2)
+    share_amount = models.DecimalField(decimal_places=6, max_digits=7+6)
+    total_amount = models.DecimalField(decimal_places=2, max_digits=7+2)
+    trade_type = models.BooleanField(choices=(
+        (True, 'Buy'),
+        (False, 'Sell')
+    ))
