71bcb6041bf194cebdf3a53a437337b8d51b1da5
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-03 15:20:06 -0500

    Create `DataTypeValidator`

--- stocks/management/commands/import_stock_trades_utilities/RobinhoodStockTradeMapper.py
+++ stocks/management/commands/import_stock_trades_utilities/RobinhoodStockTradeMapper.py
@@ -129,3 +129,12 @@ class FractionalShareTrade:
                          self.share_price,
                          self.total_amount,
                          str(self.datetime)])
+
+
+class DataTypeValidator:
+    @staticmethod
+    def validate_data_type(variable, data_type, exception_error_message):
+        if isinstance(variable, data_type):
+            return variable
+        else:
+            raise ValueError(exception_error_message)
