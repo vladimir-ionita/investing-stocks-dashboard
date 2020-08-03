79eabdc200350d0d2ee42efcd9dfe7ea758262e7
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-03 14:22:09 -0500

    Make `StockSymbol`'s name blank-able

--- stocks/models.py
+++ stocks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class StockSymbol(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=True)
     symbol = models.CharField(max_length=5)
 
     def __str__(self):
