db8ca73fa09c2b585b2f0319a4dcbd2fecff8ede
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-03 15:24:55 -0500

    Implement data validation for `FractionalShareTrade`

--- stocks/management/commands/import_stock_trades_utilities/RobinhoodStockTradeMapper.py
+++ stocks/management/commands/import_stock_trades_utilities/RobinhoodStockTradeMapper.py
@@ -116,12 +116,12 @@ class WholeShareTrade:
 
 class FractionalShareTrade:
     def __init__(self, stock_symbol, trade_type, share_amount, share_price, total_amount, time):
-        self.stock_symbol = stock_symbol
-        self.trade_type = trade_type
-        self.share_amount = share_amount
-        self.share_price = share_price
-        self.total_amount = total_amount
-        self.datetime = time
+        self.stock_symbol = DataTypeValidator.validate_data_type(stock_symbol, str, 'Stock symbol must be a string.')
+        self.trade_type = DataTypeValidator.validate_data_type(trade_type, TradeType, 'Trade type must be a TradeType.')
+        self.share_amount = DataTypeValidator.validate_data_type(share_amount, float, 'Share amount must be a float.')
+        self.share_price = DataTypeValidator.validate_data_type(share_price, float, 'Share price must be a float.')
+        self.total_amount = DataTypeValidator.validate_data_type(total_amount, float, 'Total amount must be float.')
+        self.datetime = DataTypeValidator.validate_data_type(time, datetime, 'Time must be a datetime.')
 
     def __str__(self):
         return ' '.join([self.trade_type.name,
