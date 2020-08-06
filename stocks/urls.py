f455fde75571f56fcb85e9fe11e571270097c572
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-06 10:56:11 -0500

    Create `home_page_view`

--- stocks/urls.py
+++ stocks/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'stocks'
 
 urlpatterns = [
+    path('', views.home_page_view, name='home'),
     path('stocks/', views.stocks_page_view, name='stocks')
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
+
+def home_page_view(request):
+    return redirect(reverse('stocks:stocks'), permanent=True)
 
 
 def stocks_page_view(request):
