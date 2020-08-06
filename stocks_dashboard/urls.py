be76938f5ef42fbd61bded10bfdd3115d34aeee8
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-06 10:54:49 -0500

    Create stocks app `urls.py`

--- stocks_dashboard/urls.py
+++ stocks_dashboard/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 admin.site.site_header = "Stocks Investment Portfolio"
 admin.site.site_title = "Stocks Investment Portfolio"
@@ -22,4 +22,5 @@ admin.site.index_title = "Dashboard"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('stocks.urls'))
 ]
