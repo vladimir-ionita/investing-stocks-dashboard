6837b7037cdd71172fb565b3cd850366f0e0a952
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-08-06 18:50:58 -0500

    Implement `stocks_page_view`

--- stocks/views.py
+++ stocks/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render, redirect, reverse
 
+from . import models
+
 
 def home_page_view(request):
     return redirect(reverse('stocks:stocks'), permanent=True)
 
 
 def stocks_page_view(request):
-    return render(request, "stocks/stocks.html", {})
+    stock_symbol_list = models.StockSymbol.objects.all()
+    context_stocks = []
+    for stock_symbol in stock_symbol_list:
+        stock_description = {
+            'stock': stock_symbol.symbol,
+            'shares': stock_symbol.shares_owned,
+            'investments': stock_symbol.investments_made,
+            'cheapest_share': stock_symbol.cheapest_share_price,
+            'most_expensive_share': stock_symbol.most_expensive_share_price
+        }
+        context_stocks.append(stock_description)
+
+    context = {
+        'stocks_description_fields': [
+            'Stock',
+            'Shares',
+            'Investments*',
+            'Cheapest share price',
+            'Most expensive share'
+        ],
+        'stocks_description': context_stocks
+    }
+
+    return render(request, "stocks/stocks.html", context)
